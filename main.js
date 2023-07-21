ff0fe6d2534d4d049516a141a000243577d63d14
cristian <crissnar12xd@gmail.com>
2023-07-21 18:45:37 -0500

    :v

---
+++ main.js
@@ -0,0 +1,31 @@
+// let boton=document.getElementById('Boton')
+// let año=document.getElementById('año')
+// let mes=document.getElementById('mes')
+let texto=document.getElementById('contenido')
+// let trimestre=document.getElementById('trimestre')
+
+
+
+
+
+function datos (mes,año,trimestre,radio){
+    let arreglo=[mes,año,trimestre,radio]
+    console.log(arreglo)
+
+    // arreglo.forEach(element => {
+    //     texto.textContent=`${element}`
+    // });
+
+
+    // console.log(arreglo)
+    return arreglo;
+
+
+}
+
+
+
+
+
+
+
